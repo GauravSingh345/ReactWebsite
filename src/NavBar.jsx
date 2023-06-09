2002254b3447f3a3fbc31d3e52c0b4e39fe95e48
Gaurav Singh <108728739+GauravSingh345@users.noreply.github.com>
2023-06-09 10:05:53 +0530

    Update NavBar.jsx

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
        
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <NavLink  className="navbar-brand" to="/">A2zsaas</NavLink>
+    <NavLink  className="navbar-brand" to="/">Unicloud It Services</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
