f2351d69641c6f78eec45f3fd1633f666998a490
Gaurav Singh <108728739+GauravSingh345@users.noreply.github.com>
2023-06-09 10:04:35 +0530

    Update Common.jsx

--- src/Common.jsx
+++ src/Common.jsx
@@ -9,7 +9,7 @@ const Common = (props) => {
               <div className="col-10 mx-auto">
               <div className="row">
                 <div className="col-md-6 pt-lg-0 order-2 order-lg-1 d-flex  justify-content-center  flex-column">
-                <h1>{props.name} <strong className='brand-name'>A2zsaas</strong></h1>
+                <h1>{props.name} <strong className='brand-name'>UniCloud It Services</strong></h1>
                 <h2 className="my-3">
                   we are the team of talented developer making websites
                 </h2>
